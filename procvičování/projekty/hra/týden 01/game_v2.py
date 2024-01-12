79a964dcfe4a46cb97daada4fe55ba452364157a
JS <souhrada@trebesin.cz>
2024-01-12 15:25:35 +0100

    updated week2

--- procvičování/projekty/hra/týden 01/game_v2.py
+++ procvičování/projekty/hra/týden 01/game_v2.py
@@ -4,6 +4,7 @@ from sys import exit
 # inicializuje hru - spustíme pygame
 pygame.init()
 
+# vytvoř hodiny
 clock = pygame.time.Clock()
 
 # naše proměnné, které udávají výšku a šířku
@@ -12,11 +13,12 @@ screen_width = 800
 # vytvoříme obraz
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-
+# vytvoření surface pro postavičku hráče - načtení obrázku
 player_surf = pygame.image.load("player_sprite.png").convert_alpha()
 player_x = 100
 player_y = 200
 
+# vytvoření surface pro postavičku monster - nepřítele - načtení obrázku
 monster_surf = pygame.image.load("monster_sprite.png").convert_alpha()
 
 
@@ -32,7 +34,7 @@ while True:
     # proměnná key, pod ní schováme stisknutou klávesu
     key = pygame.key.get_pressed()
 
-    # pokud je stisknutá šipka doleva
+    # pokud je stisknutá šipka doleva, atd.
     if key[pygame.K_LEFT]:
         player_x -= 10
     elif key[pygame.K_RIGHT]:
@@ -45,9 +47,13 @@ while True:
     # obarví obrazovku na bílo
     screen.fill((255, 255, 255))
 
+    # na screen vykresli - surface hráče, na x,y
     screen.blit(player_surf, (player_x, player_y))
+    # na screen vykresli - surface monstra, na x,y
     screen.blit(monster_surf, (300, 500))
 
     # updatuje vše
     pygame.display.update()
+
+    # omez tickrate (jak rychle hra poběží) na 60 fps
     clock.tick(60)
