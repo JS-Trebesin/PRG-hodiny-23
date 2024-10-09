bd8843a019ce3d07e082b0c4d7ecd6743be645dc
JS <souhrada@trebesin.cz>
2024-10-09 10:24:22 +0200

    +9.10. v1

--- 00 Python/10 flask/flask_app.py
+++ 00 Python/10 flask/flask_app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template("index.html")
 
 @app.route("/bye")
-def bye():
+def bye(): 
     return render_template("bye.html")
+
+@app.route("/form")
+def form():
+    name = request.args.get("name", default="_____")
+    input_class = request.args.get("class", default="_____")
+    message = request.args.get("message", default="_____")
+    return render_template("form.html", name=name, form_class=input_class, message=message)
+
+
+
 
 
 if __name__ == "__main__":
