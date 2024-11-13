aa3c95b8aab26164212980fbb75149afcb217e20
JS <souhrada@trebesin.cz>
2024-11-13 15:08:15 +0100

    +13.11 + komentáře k flaskovinám

--- 00 Python/11 sqlite/zapis.py
+++ 00 Python/11 sqlite/zapis.py
@@ -8,7 +8,7 @@ cursor = connection.cursor()
 
 input_color = input("Add house color: ")
 
-#zápis do databáze
+# zápis do databáze
 cursor.execute(
     "INSERT INTO houses (color) VALUES (?)", (input_color,)
 )
@@ -16,8 +16,11 @@ cursor.execute(
 #potvrzení vložení dat do databáze
 connection.commit()
 
-
+# výběr dat z databáze pomocí kurzoru
 cursor.execute("SELECT * FROM houses")
+# fetchall() data získá a umožní nám je uložit pod proměnnou
+# data se vrací jako list jednotlivých rows (řad) z databáze, kde data v řadě jsou v tuple 
+# [(data za řady 1, další data z řady 1), (data z řady 2, další data z řady 2)]
 rows = cursor.fetchall()
 
 for row in rows:
