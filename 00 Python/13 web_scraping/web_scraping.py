5f3acf5b180b03a46fca706b0f5f44df9ce47f19
JS <souhrada@trebesin.cz>
2025-01-15 10:52:57 +0100

    + 15.1

---
+++ 00 Python/13 web_scraping/web_scraping.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def main():
+
+    url = "https://www.trebesin.cz"
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    paragraph = soup.p
+
+    print(f"První odstavec na stránce je: {paragraph.text}")
+
+    # all_p = soup.find_all("p")
+
+    # for p in all_p:
+    #     print(p.text)
+
+    gym = soup.find(id="favimagehover-title4")
+    print(f"Název oboru je {gym.text}")
+    gym2 = soup.select("#favimagehover-title4")
+
+if __name__ == "__main__":
+    main()
